a89b83c3cdc56dd050099b858f5414c89c8304bb
Telis <telisjok@yahoo.com>
2017-05-26 19:00:03 +0300

    Set on item click listener

--- app/src/main/java/gr/ihu/lab/ihuweather_01/ForecastFragment.java
+++ app/src/main/java/gr/ihu/lab/ihuweather_01/ForecastFragment.java
@@ -11,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -148,6 +150,17 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                final String Item = adapter.getItem(position);
+
+                Toast toast = Toast.makeText(getActivity(), Item, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
 
         return rootView;
     }
